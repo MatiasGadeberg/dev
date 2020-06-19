57ba9d5e2bba7fa1ee2077c09b6f55f1f5266707
HMGD (Matias Schaff Gadeberg) <hmgd@novonordisk.com>
2020-06-20 00:26:45 +0200

    added strength only scraping, created plot

--- python/bs4test.py
+++ python/bs4test.py
@@ -1,24 +1,24 @@
 import requests
 from bs4 import BeautifulSoup
 import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
 
-print('import done')
 # get the data
 maindata = requests.get('https://dota2.gamepedia.com/Dota_2_Wiki')
 
 # load data into bs4
 mainsoup = BeautifulSoup(maindata.text, 'html.parser')
-print('maindata recieved')
 
 baseurl = "https://dota2.gamepedia.com/"
 
-col_names = ['Hero','str','agi','int']
 # get data from scrape
+
+data = mainsoup.find('a', {'title': 'Strength'}).find_parent('tr').next_sibling.next_sibling.find_all('div', {'class': 'heroentry'})
+
 heroes = []
-print('starting iterations')
+
+#for div in mainsoup.find_all('div', {'class': 'heroentry'}):
 for div in mainsoup.find('a', {'title': 'Strength'}).find_parent('tr').next_sibling.next_sibling.find_all('div', {'class': 'heroentry'}):
+    
     hero = []
     hero.append(div.a['title'])
     herourl = baseurl + '_'.join(div.a['title'].split(" "))
@@ -27,11 +27,9 @@ for div in mainsoup.find('a', {'title': 'Strength'}).find_parent('tr').next_sibl
     soup = BeautifulSoup(data.text, 'html.parser')
     
     for b in soup.find('table', {'class': 'infobox'}).find_all('b'):
-        hero.append(int(b.text))
+        hero.append(b.text)
 
 
-    heroes.append(hero) 
+    heroes.append(hero)
 
-df = pd.DataFrame(heroes, columns = col_names)
-ax = df.plot.bar(x='Hero', y=['str','agi','int'], color = ['r','g','b'], subplots=True)
-plt.show()
+print(heroes)
